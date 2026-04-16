710a17c4a913e6562a228ef167d4505b201fab9d
xianzf <xianzf@geneplus.org.cn>
2026-04-17 00:22:01 +0800

    feat(ci): 添加GitHub Pages部署工作流和环境配置
    添加.env环境文件示例和测试/生产配置
    创建GitHub Actions工作流用于自动部署到Pages
    修改vite配置以支持环境变量加载和项目基础URL设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 // 自动引入插件
@@ -14,37 +14,53 @@ const pathResolve = (path: string) => {
 }
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
-      resolvers: [
-        ElementPlusResolver({ importStyle: 'sass' }),
-        // 自动导入图标组件
-        IconsResolver({
-          prefix: 'Icon'
-        })
-      ]
-    }),
-    Components({
-      dirs: ['src/components'],
-      resolvers: [
-        // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ['ep']
-        }),
-        ElementPlusResolver({ importStyle: 'sass' })
-      ]
-    }),
-    Icons({
-      autoInstall: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': pathResolve('./src')
+export default defineConfig(({ mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有
+  // `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return {
+    base: env.VITE_BASE_PROJECT_URL || '/',
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: env.VITE_BASE_API_URL,
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/api/, '')
+    //     }
+    //   }
+    // },
+    plugins: [
+      vue(),
+      vueDevTools(),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+        resolvers: [
+          ElementPlusResolver({ importStyle: 'sass' }),
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon'
+          })
+        ]
+      }),
+      Components({
+        dirs: ['src/components'],
+        resolvers: [
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep']
+          }),
+          ElementPlusResolver({ importStyle: 'sass' })
+        ]
+      }),
+      Icons({
+        autoInstall: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': pathResolve('./src')
+      }
     }
   }
 })
